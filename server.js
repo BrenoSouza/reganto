b0e584b5ea971034ef5bbfe59023f23c58560c34
victor <vhfsfox@gmail.com>
2016-03-12 12:27:15 -0300

    correção do problema com os arquivos estaticos

--- server.js
+++ server.js
@@ -2,189 +2,52 @@
 //  OpenShift sample Node application
 var express = require('express');
 var fs      = require('fs');
+var path = require('path');
+
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+
+// Main application.
+var app = express();
+
+// Produção application.
+var producao = express();  
+producao.get('/', function (req, res) {  
+  res.sendfile('./producao/html/index.html');
+});
+
+// Breno application.
+var breno = express();  
+breno.get('/', function (req, res) {
+    res.sendfile('./user-breno/html/index.html');  
+});
+
+// Rafaela application.
+var rafaela = express();  
+rafaela.get('/', function (req, res) {
+    res.sendfile('./user-rafaela/html/index.html');  
+});
+
+// Victor application.
+var victor = express();  
+victor.get('/', function (req, res) {
+    res.sendfile('./user-victor/html/index.html');  
+});
+
+// Seting static files path
+app.use('/', express.static(__dirname + '/producao/html/'));
+app.use('/breno', express.static(__dirname + '/user-breno/html/'));
+app.use('/rafaela', express.static(__dirname + '/user-rafaela/html/'));
+app.use('/victor', express.static(__dirname + '/user-victor/html/'));
+
+// Mouting applications.
+app.use('/', producao);  
+app.use('/breno', breno);
+app.use('/rafaela', rafaela);
+app.use('/victor', victor);
 
 
-/**
- *  Define the sample application.
- */
-var SampleApp = function() {
-
-    //  Scope.
-    var self = this;
-
-
-    /*  ================================================================  */
-    /*  Helper functions.                                                 */
-    /*  ================================================================  */
-
-    /**
-     *  Set up server IP address and port # using env variables/defaults.
-     */
-    self.setupVariables = function() {
-        //  Set the environment variables we need.
-        self.ipaddress = process.env.OPENSHIFT_NODEJS_IP;
-        self.port      = process.env.OPENSHIFT_NODEJS_PORT || 8080;
-
-        if (typeof self.ipaddress === "undefined") {
-            //  Log errors on OpenShift but continue w/ 127.0.0.1 - this
-            //  allows us to run/test the app locally.
-            console.warn('No OPENSHIFT_NODEJS_IP var, using 127.0.0.1');
-            self.ipaddress = "127.0.0.1";
-        };
-    };
-
-
-    /**
-     *  Populate the cache.
-     */
-    self.populateCache = function() {
-        if (typeof self.zcache === "undefined") {
-            self.zcache = { 'index.html': '' };
-        }
-
-        //  Local cache for static content.
-        self.zcache['index.html'] = fs.readFileSync('./index.html');
-    };
-
-
-    /**
-     *  Retrieve entry (content) from cache.
-     *  @param {string} key  Key identifying content to retrieve from cache.
-     */
-    self.cache_get = function(key) { return self.zcache[key]; };
-
-
-    /**
-     *  terminator === the termination handler
-     *  Terminate server on receipt of the specified signal.
-     *  @param {string} sig  Signal to terminate on.
-     */
-    self.terminator = function(sig){
-        if (typeof sig === "string") {
-           console.log('%s: Received %s - terminating sample app ...',
-                       Date(Date.now()), sig);
-           process.exit(1);
-        }
-        console.log('%s: Node server stopped.', Date(Date.now()) );
-    };
-
-
-    /**
-     *  Setup termination handlers (for exit and a list of signals).
-     */
-    self.setupTerminationHandlers = function(){
-        //  Process on exit and signals.
-        process.on('exit', function() { self.terminator(); });
-
-        // Removed 'SIGPIPE' from the list - bugz 852598.
-        ['SIGHUP', 'SIGINT', 'SIGQUIT', 'SIGILL', 'SIGTRAP', 'SIGABRT',
-         'SIGBUS', 'SIGFPE', 'SIGUSR1', 'SIGSEGV', 'SIGUSR2', 'SIGTERM'
-        ].forEach(function(element, index, array) {
-            process.on(element, function() { self.terminator(element); });
-        });
-    };
-
-
-    /*  ================================================================  */
-    /*  App server functions (main app logic here).                       */
-    /*  ================================================================  */
-
-    /**
-     *  Create the routing table entries + handlers for the application.
-     */
-    self.createRoutes = function() {
-        self.routes = { };
-
-        self.routes['/asciimo'] = function(req, res) {
-            var link = "http://i.imgur.com/kmbjB.png";
-            res.send("<html><body><img src='" + link + "'></body></html>");
-        };
-
-        self.routes['/'] = function(req, res) {
-            res.setHeader('Content-Type', 'text/html');
-            //res.send(self.cache_get('index.html') );
-            self.app.set('/', __dirname + '/producao/html/');
-            self.app.use('/', express.static( __dirname + '/producao/html/' ));
-            res.sendfile('./producao/html/index.html');
-           
-        };
-
-        self.routes['/victor'] = function(req, res) {
-            res.setHeader('Content-Type', 'text/html');
-            //res.send(self.cache_get('index.html') );
-            self.app.set('/', __dirname + '/user-victor/html/');
-            self.app.use('/', express.static( __dirname + '/user-victor/html/' ));
-            res.sendfile('./user-victor/html/index.html');
-           
-        };
-
-        self.routes['/rafaela'] = function(req, res) {
-            res.setHeader('Content-Type', 'text/html');
-            //res.send(self.cache_get('index.html') );
-            self.app.set('/', __dirname + '/user-rafaela/html/');
-            self.app.use('/', express.static( __dirname + '/user-rafaela/html/' ));
-            res.sendfile('./user-rafaela/html/index.html');
-           
-        };
-
-        self.routes['/breno'] = function(req, res) {
-            res.setHeader('Content-Type', 'text/html');
-            //res.send(self.cache_get('index.html') );
-            self.app.set('/', __dirname + '/user-breno/html/');
-            self.app.use('/', express.static( __dirname + '/user-breno/html/' ));
-            res.sendfile('./user-breno/html/index.html');
-           
-        };
-    };
-
-
-    /**
-     *  Initialize the server (express) and create the routes and register
-     *  the handlers.
-     */
-    self.initializeServer = function() {
-        self.createRoutes();
-        self.app = express.createServer();
-
-        //  Add handlers for the app (from the routes).
-        for (var r in self.routes) {
-            self.app.get(r, self.routes[r]);
-        }
-    };
-
-
-    /**
-     *  Initializes the sample application.
-     */
-    self.initialize = function() {
-        self.setupVariables();
-        self.populateCache();
-        self.setupTerminationHandlers();
-
-        // Create the express server and routes.
-        self.initializeServer();
-    };
-
-
-    /**
-     *  Start the server (starts up the sample application).
-     */
-    self.start = function() {
-        //  Start the app on the specific interface (and port).
-        self.app.listen(self.port, self.ipaddress, function() {
-            console.log('%s: Node server started on %s:%d ...',
-                        Date(Date.now() ), self.ipaddress, self.port);
-        });
-    };
-
-};   /*  Sample Application.  */
-
-
-
-/**
- *  main():  Main code.
- */
-var zapp = new SampleApp();
-zapp.initialize();
-zapp.start();
-
+// Start listening.
+app.listen(server_port, server_ip_address, function(){
+  console.log("Listening on " + server_ip_address + ", server_port " + server_port)
+});
