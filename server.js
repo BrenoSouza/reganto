d3b3c7de1251f2055aa8fc1765d9884e76c47c3e
victor <vhfsfox@gmail.com>
2016-04-02 12:05:29 -0300

    Integracao com o github

--- server.js
+++ server.js
@@ -34,18 +34,40 @@ victor.get('/', function (req, res) {
     res.sendfile('./user-victor/html/index.html');  
 });
 
+// Victor application.
+var api = express();  
+api.get('/', function (req, res) {
+	teste = {
+		"dog": ["Dog", "Cane", "Perro", "Hund", "Chien"],
+		"cat": ["Cat", "Gatto", "Gato", "Katze", "Chat"],
+		"flower": ["Flower", "Fiore", "Flor", "Blume", "Fleur"],
+		"coffee": ["Coffee", "Caff\u00e8", "Caf\u00e9", "Kaffee"],
+		"c2": ["Construct 2"]
+	}
+    res.send("{:666}");  
+});
+
 // Seting static files path
 app.use('/', express.static(__dirname + '/producao/html/'));
 app.use('/breno', express.static(__dirname + '/user-breno/html/'));
 app.use('/rafaela', express.static(__dirname + '/user-rafaela/html/'));
 app.use('/victor', express.static(__dirname + '/user-victor/html/'));
 
+
 // Mouting applications.
-app.use('/', producao);  
+app.use('/', producao);
+app.use('/api', api);  
 app.use('/breno', breno);
 app.use('/rafaela', rafaela);
 app.use('/victor', victor);
 
+app.use(function(req, res, next) {
+	res.setHeader('Access-Control-Allow-Origin', '*');
+	res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
+	res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type, Authorization');
+	//req.setHeader('Authorization');
+	next();
+});
 
 // Start listening.
 app.listen(server_port, server_ip_address, function(){
